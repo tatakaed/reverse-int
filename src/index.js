b00be7f1b230f7469b5185f1ea2c782c87ae5b1c
Ivan <vaniamar@mail.ru>
2024-07-31 20:42:42 +0700

    feat: solution reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n > 0) {
+        const stringNum = Array.from(String(n), Number).reverse().join('')
+        const reverseNum = Number(stringNum)
+        return reverseNum
+    }
+    else {
+        const nonNegativeNumber = n.toString().slice(1).split('')
+        return nonNegativeNumber.reverse().join('')
+    }
+
 }
+
+
